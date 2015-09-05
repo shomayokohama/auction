613d94d74b0a4e1fbe7f1bae29642b054f9234db
横浜翔馬 <yokohamashoma@yokohama.local>
2015-09-05 18:02:42 +0900

    バンドルインストールした

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # get '/items' => 'items#index'
   # post 'items' => 'items#create'
   resources :items
+  root 'items#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
