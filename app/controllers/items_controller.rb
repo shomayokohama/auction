44f7d60ca42563b81b96e0bc9db3f656b5c0de39
横浜翔馬 <yokohamashoma@yokohama.local>
2015-09-05 15:24:56 +0900

    新規登録2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,7 +8,26 @@ class ItemsController < ApplicationController
   end
 
   def new
-    @items = Item.new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to "/items/#{@item.id}"
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :name,
+      :price,
+      :seller_id,
+      :description,
+      :email,
+      :image_url
+      )
   end
 
 end
